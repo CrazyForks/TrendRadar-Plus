1bf111fdd047ea38be1dc51cf69d638598eb57b3
sundt <sundtao@gmail.com>
2026-03-08 21:58:27 +0800

    deploy: 2026-03-08 21:58:27

--- hotnews/web/static/js/src/subscription.js
+++ hotnews/web/static/js/src/subscription.js
@@ -192,9 +192,7 @@ async function loadSubscriptionPlans() {
                 ${plans.map((plan, idx) => renderSubscriptionPlanCard(plan, idx === 1)).join('')}
             </div>
             <div class="subscription-benefits">
-                <div class="subscription-benefit">✓ 专属自定义主题追踪</div>
-                <div class="subscription-benefit">✓ 第一时间获取关注新闻</div>
-                <div class="subscription-benefit">✓ 重点关注即时送达</div>
+                <div class="subscription-benefit">✓ 专属自定义主题追踪 &nbsp;&nbsp; ✓ 第一时间获取关注新闻 &nbsp;&nbsp; ✓ 重点关注即时送达</div>
             </div>
         `;
         
