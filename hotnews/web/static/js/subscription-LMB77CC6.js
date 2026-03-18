2f5de3e4ba265a59bc65ac86a619fe0a2700bb44
sundt <sundtao@gmail.com>
2026-03-18 10:09:34 +0800

    feat: 微信扫码登录后返回是否需要绑定邮箱状态

---
+++ hotnews/web/static/js/subscription-LMB77CC6.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-HLHWK4RF.js";import"./chunk-N3WOZHU5.js";export{c as closeSubscriptionModal,a as getSubscriptionStatus,b as openSubscriptionModal};
--- hotnews/web/static/js/subscription-XGJQLDCS.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c}from"./chunk-BSEEVU7V.js";import"./chunk-YRLDCOZY.js";export{c as closeSubscriptionModal,a as getSubscriptionStatus,b as openSubscriptionModal};
